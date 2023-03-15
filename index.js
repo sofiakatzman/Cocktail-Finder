14b49f1d37da488bd1f4de7c817f16cf54c1dd35
Sofia <katzmansof@gmail.com>
2023-03-15 15:48:11 -0400

    Wrote simple pseudo code for base of application functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+//Cocktail Recipie Finder BASIC FUNCTION
+
+//This app will be connected to an API in file name JSON.db
+//When the DOM Content Loads, the webpage will ask the user to pick up to 3 ingredients 
+//When the user submits the form, JS will look through the DB for recipies that match those X ingredients 
+//All matches will be returned to the user. 
+
+//The user can choose to identify the maximum number of additional ingredients they will need in order to make their creation 
+//JS will return the recipie that meets that specific criteria (array length < x )
+
+//The recipie cards will have a "like button"
+//when a user likes a recipie the like is sent via server and stored in the JSON.db file
+
+//night mode will change the css class to an inverted design that is tbd 
+//night mode will be availabel via a button or switch - tbd 
+
+
