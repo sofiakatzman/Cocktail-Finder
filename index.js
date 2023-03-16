5bfc8ae482d72c525a7aba1d99f99dfb6789a3e4
Sofia Katzman <katzmansof@gmail.com>
2023-03-15 23:10:23 -0400

    created DOM elements to display recipe cards on webpage

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 document.addEventListener("DOMContentLoaded", function() {});
 
 //sends request for data from server
-const url = "http://localhost:3000/recipies"
+const url = "http://localhost:3000/recipes"
 
 //DOM element selectors that collect user filter labels
 const form = document.querySelector('form')
 const dropdownSpiritBase = document.querySelector('#dropdownSpiritBase')
 const dropdownMixer = document.querySelector('#dropdownMixer')
-
+let divCollect = document.querySelector('#recipe-display')
 
 //event listener that waits for button to be clicked to store the filter label data
 form.addEventListener('submit', (event) => {
@@ -24,7 +24,8 @@ form.addEventListener('submit', (event) => {
   //filter function that itterates through kson data
   //need to add if statements for when only one or the other is picked
         if(recipe.spirit1[0] === selectedSpiritBase && recipe.mixer[0] === selectedMixer){
-            console.log(recipe.name)
+          displayRecipeCard(recipe)  
+          console.log(recipe.name)
         } else {
             console.log("Please try a different combination!") // in the end we want this to return only if there are NO matched recipes
         }
@@ -33,13 +34,36 @@ form.addEventListener('submit', (event) => {
   .catch(error => console.error(error))
 })
 
+
 function displayRecipeCard(recipe){
 
-}
+  //creates a heading for drink name
+    let h2 = document.createElement('h2')
+    h2.innerHtml = recipe.name
+    h2.innerHtml = "TEST"
+  //displays drink photo
+    let img = document.createElement('img')
+    img.setAttribute('src', recipe.imageURL)
 
-//write function to display matchd item on DOM 
+  //creates a paragraph element that can hold drink ingredients
+    img.setAttribute('class', 'recipe-photo')
+    let pLikes = document.createElement('p')
+    pLikes.innerText = `${recipe.likeCount}`
+
+  //creates a paragraph element that can hold drink likes
+
+    let pIngredients = document.createElement('p')
+    pIngredients.innerText = "hello"
 
 
+  //creates like button 
+    let btn = document.createElement('button')
+    btn.setAttribute('class', 'like-btn')
+    btn.innerText = "like"
 
-
-  
+    //puts all new elements together into one card
+    let divCard = document.createElement('div')
+    divCard.setAttribute('class', 'card')
+    divCard.append(h2, img, pLikes, btn)
+    divCollect.append(divCard)
+    }
