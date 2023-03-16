3939ac3900cd6a0f249aacc41b75be5c68920e24
Sofia <katzmansof@gmail.com>
2023-03-15 20:19:00 -0400

    wrote function that goes through db data to find a match from selected ingredient in drop down

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-//Cocktail Recipie Finder BASIC FUNCTION
 
-//This app will be connected to an API in file name JSON.db
-//When the DOM Content Loads, the webpage will ask the user to pick up to 3 ingredients 
-//When the user submits the form, JS will look through the DB for recipies that match those X ingredients 
-//All matches will be returned to the user. 
-
-//The user can choose to identify the maximum number of additional ingredients they will need in order to make their creation 
-//JS will return the recipie that meets that specific criteria (array length < x )
-
-//The recipie cards will have a "like button"
-//when a user likes a recipie the like is sent via server and stored in the JSON.db file
-
-//night mode will change the css class to an inverted design that is tbd 
-//night mode will be availabel via a button or switch - tbd 
-
+//wait for dom to load to start up tbd if necessary
 document.addEventListener("DOMContentLoaded", function() {
-    // code to be executed after the DOM is loaded
   });
 
+//sends request for data from server
 const url = "http://localhost:3000/recipies"
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => getRecipiesByFilter(data))
-  .catch(error => console.error(error))
 
-function getRecipiesByFilter(data){
-    console.log(data)
-}
-
+//DOM element selectors that collect user filter labels
 const form = document.querySelector('form')
-const dropdown = document.querySelector('#dropdown')
-
-
 const dropdownSpiritBase = document.querySelector('#dropdownSpiritBase')
 const dropdownSpirit2 = document.querySelector('#dropdownSpirit2')
 const dropdownMixer = document.querySelector('#dropdownMixer')
@@ -41,6 +17,8 @@ const dropdownGarnish = document.querySelector('#dropdownGarnish')
 const dropdownGarnish2 = document.querySelector('#dropdownGarnish2')
 const submitButton = document.querySelector('#submitButton')
 
+
+//event listener that waits for button to be clicked to store the filter label data
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   const selectedSpiritBase = dropdownSpiritBase.value
@@ -49,10 +27,33 @@ form.addEventListener('submit', (event) => {
   const selectedLemonLime = dropdownLemonLime.value
   const selectedGarnish = dropdownGarnish.value
   const selectedGarnish2 = dropdownGarnish2.value
+  console.log(selectedSpiritBase)
 
-  console.log(selectedSpiritBase + selectedSpirit2 + selectedMixer + selectedLemonLime + selectedGarnish + selectedGarnish2)
+  fetch(url)
+  .then(response => response.json())
+  .then(data => { 
+    data.forEach(recipie => {
+        if (recipie.spirit1[0] === selectedSpiritBase){
+            console.log("We have a match!")
+        }
+
+        else{
+            console.log("No match!")
+        }
+        
+    })
+})
+  .catch(error => console.error(error))
 
 })
+
+//Now i need to write the function that will go through the arrays and look for the values above. 
+
+
+
+
+
+
 
 
   
